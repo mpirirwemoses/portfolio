0119f3bb6673cf0bb5b46ba39b05866c6d0dd4b8
Moses Mpirirwe <mpirirwemoses2@gmail.com>
2025-08-08 10:54:46 +0300

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import Port from './Components/port'
+import Contact from './Components/Contact'
 import './App.css'
 
-import Port from './Components/port'
-import Contact  from './Components/Contact'
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
-
 function App() {
-  
-  return(
-    
-    
-      <Routes>
-        <Route path="/" element={<Port />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    
-   
+  return (
+    <Routes>
+      <Route path="/" element={<Port />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   )
 }
 
